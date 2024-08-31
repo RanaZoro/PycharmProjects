03462e8b9d93ac80eb8ed70739fbfaa3b8edfb85
RanaZoro <asamad69000@gmail.com>
2024-09-01 00:16:23 +0500

    Update codecampOOP.py

--- pythonProject/codecampOOP.py
+++ pythonProject/codecampOOP.py
@@ -1,5 +1,6 @@
 class Item:
     pay_rate = 0.8
+    all = []
     def __init__(self,name: str,price: float,quantity=0):
         #ASSERTION:-
         '''
@@ -27,14 +28,24 @@ class Item:
         self.price = price
         self.quantity = quantity
         self.name = name
+
+        Item.all.append(self)
     def sales(self):
        return self.price * self.quantity
     def discount(self):
         return self.price * Item.pay_rate
-item1 = Item("ThinkPad",500,4)
+    def __repr__(self):
+        return f"Item({self.name},{self.price},{self.quantity})"
+item1 = Item("Phone", 100, 1)
+item2 = Item("Laptop", 1000, 3)
+item3 = Item("Cable", 10, 5)
+item4 = Item("Mouse", 50, 5)
+item5 = Item("Keyboard", 75, 5)
 print(item1.name)
 print(item1.sales())
 print(int(item1.discount()))
 #print(Item.__dict__,item1.__dict__)#Checking The Attributes Of Class Vars And Instance Vars Respectively in dict
+for instance in Item.all:
+    print(instance.name,end=" ")
 
-#continue from 41:55
+#continue from 51:00
