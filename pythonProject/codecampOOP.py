84888fda0c8d6d15d446afa6bd956cb3095d3971
Rana Abdus Samad <167080832+RanaZoro@users.noreply.github.com>
2024-09-30 18:47:06 +0500

    Update

--- pythonProject/codecampOOP.py
+++ pythonProject/codecampOOP.py
@@ -25,15 +25,13 @@ class Item:
         '''
         assert price >= 0
         assert quantity >=0
-        self.price = price
+        self.__price = price
         self.quantity = quantity
-        self._name = name
+        self.__name = name #adding two inderscores jsut makes it a private attribute
 
         Item.all.append(self)
     def sales(self):
-       return self.price * self.quantity
-    def discount(self):
-        return self.price * Item.pay_rate
+       return self.__price * self.quantity
     @classmethod
     def clsmethod(cls):
         return "Class Methods Are Used When We Need To Work With Something That's Not An Instance Of A Class"
@@ -54,12 +52,23 @@ class Item:
         else:
             raise ValueError("The input must be an integer or a float.")
     def __repr__(self):
-        return f"{self.__class__.__name__}({self.name},{self.price},{self.quantity})"
-    @property #Getters Are Immutable After Initialization
-    def read_only(self):
-        return self._name
+        return f"{self.__class__.__name__}({self.name},{self.__price},{self.quantity})"
+    @property
+    def price(self):
+        return self.__price
+    def discount(self):
+        return self.__price * Item.pay_rate
+    def increment(self, increper):
+        return self.__price + self.__price * increper
 
-item1 = Item("Phone", 100, 1)
+    @property #Getters Are Immutable After Initialization
+    def name(self):
+        return self.__name
+    @name.setter # you can use getters and setter to execute specific code with it when  taking or  asssinging value to a variable
+    def name(self,value):
+        self.__name = value
+
+item1 = Item("Phone", 120, 1)
 item2 = Item("Laptop", 1000, 3)
 item3 = Item("Cable", 10, 5)
 item4 = Item("Mouse", 50, 5)
@@ -101,7 +110,35 @@ phone1.real_price()
 
 print(Phone("IPhone 13 Pro",800.0,8,2))
 print(Item("IPhone 13",800.0,8))
+print(item1.increment(0.3))
 
-print("test")
-
-#continue from 1:30:12
+# 1) Encapsulation = Restricting Attributes To Be Accessed Outside The Class
+# 2) Abstraction = Only Shows Necessary Info Hides Unnecessary Info :3
+#For Example:-
+class Example:
+    def __foo(self):
+        test_num = 5+89
+        return print(test_num)
+    def result(self):
+        self.__foo()
+ex1 = Example()
+ex1.result()
+#Inheritance = Child Classes Inheriting From Parents
+#Polymorphism =  The word "polymorphism" means "many forms", and in programming it refers to methods/functions/operators with the same name that can be executed on many objects or classes.
+#For Example;-
+class Keyboard(Item):
+    all = []
+    def __init__(self, name: str, price: float, quantity=0, broken_boards=0, fquantity=0):
+        super().__init__(
+            name, price, quantity
+        )
+        assert price >= 0
+        assert quantity >= 0
+        assert broken_boards >= 0
+        self.broken_boards = broken_boards
+        self.fquantity = fquantity
+        Keyboard.all.append(self) #Can Be Removed
+    def real_price(self):
+        pass
+keyboard1 = Keyboard("GXT Gaming Keyboard",3400)
+print(keyboard1.discount())
