95438216e95537022c536de00045582535931a2c
Rana Abdus Samad <167080832+RanaZoro@users.noreply.github.com>
2024-12-15 12:03:41 +0500

    Update

--- pythonProject/CS50 _FILEIO.py
+++ pythonProject/CS50 _FILEIO.py
@@ -1,1 +1,11 @@
-print("test")
+#NEW THINGS
+txt = "     banana     "
+
+x = txt.rstrip()
+
+print("of all fruits", x, "is my favorite")
+#sort
+rabbit = ["General Munchkins", "Old Carrot", "Mr. Rabbler"]
+for rabbits in sorted(rabbit):
+    print(f"hello, {rabbits}!")
+ #37:48
