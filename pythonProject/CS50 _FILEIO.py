bf1f6a2095eb0c11d3389ac66f0ca4a0c4af05d7
Rana Abdus Samad <167080832+RanaZoro@users.noreply.github.com>
2024-12-24 00:25:14 +0500

    Update

--- pythonProject/CS50 _FILEIO.py
+++ pythonProject/CS50 _FILEIO.py
@@ -1,4 +1,6 @@
 #NEW THINGS
+from PIL import Image
+import sys
 txt = "     banana     "
 
 x = txt.rstrip()
@@ -9,3 +11,11 @@ rabbit = ["General Munchkins", "Old Carrot", "Mr. Rabbler"]
 for rabbits in sorted(rabbit):
     print(f"hello, {rabbits}!")
  #37:48
+ #MAKING GIFS
+images = []
+for arg in sys.argv[1:]:
+         image = Image.open(arg)
+         images.append(image)
+images[0].save(
+    "costumes.gif", save_all=True, loop = 0, append_images = [images[1]], duration = 200
+)#works in vs i guess
